c298c570f1edf4f0e69509b2a215358d1eec57e6
diaamant <diaamantkia@gmail.com>
2022-11-12 09:56:02 +0300

    practic01

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -7,9 +7,24 @@ class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
 
-        return HttpResponse('<ul>'
-                            '<li>Установить python</li>'
-                            '<li>Установить django</li>'
-                            '<li>Запустить сервер</li>'
-                            '<li>Порадоваться результату</li>'
-                            '</ul>')
+
+        todo_set = {'Установить python',
+                     'Установить django',
+                      'Запустить сервер',
+                      'Порадоваться результату'}
+
+        todo_dict =  dict.fromkeys(todo_set, 0)  # Dict хранит данные в случайном порядке
+
+        resp = [f'<li>{key}</li>' for key in todo_dict]
+        resp.insert(0, '<ul>')
+        resp.append('</ul>')
+
+        return HttpResponse(resp)
+
+
+        # return HttpResponse('<ul>'
+        #                     '<li>Установить python</li>'
+        #                     '<li>Установить django</li>'
+        #                     '<li>Запустить сервер</li>'
+        #                     '<li>Порадоваться результату</li>'
+        #                     '</ul>')
