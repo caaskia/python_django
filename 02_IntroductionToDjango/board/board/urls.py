2bf844fd4b42807e31e4800ecc52691c17dcf7f7
diaamant <diaamantkia@gmail.com>
2022-11-29 19:36:17 +0300

    Practic-4.7-2

--- 02_IntroductionToDjango/board/board/urls.py
+++ 02_IntroductionToDjango/board/board/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('advertisement.urls')),
     path('',include('categories.urls')),
     path('',include('main.urls')),
     path('',include('advertisements.urls')),
