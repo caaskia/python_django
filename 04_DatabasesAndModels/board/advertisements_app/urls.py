4f4c815fbdfb7c7ef5158bbaeba33cf6ee9d266c
Alexey <alexei@bk.ru>
2022-12-13 06:26:35 +0300

    fix

--- 04_DatabasesAndModels/board/advertisements_app/urls.py
+++ 04_DatabasesAndModels/board/advertisements_app/urls.py
@@ -3,6 +3,6 @@ from .views import AdvertisementsListView, AdvertisementsDetailView
 
 urlpatterns = [
 path('advertisements/', AdvertisementsListView.as_view(), name ='advertisements'),
-path('advertisements/<int:pk>', AdvertisementsDetailView.as_view(), name ='advertisements-detail'),
+path('advertisements/<int:pk>/', AdvertisementsDetailView.as_view(), name ='advertisements-detail'),
 ]
 
